f9d3add9abd76d9fc0da233e6d4e210487a00776
陈曦 <chenxi.sean@bytedance.com>
2021-05-25 16:38:42 +0800

    add service
    Change-Id: Ic10bb0d92ead4fccc36e8449961392bcf3800ff6

--- main/main.go
+++ main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/SeanChan0901/g33rpc"
 	"log"
 	"net"
@@ -9,12 +8,27 @@ import (
 	"time"
 )
 
+type Foo int
+
+type Args struct {Num1, Num2 int}
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
+	var foo Foo
+	if err := g33rpc.Register(&foo); err != nil {
+		log.Fatal("register error", err)
+	}
+
+	// pick a free port
 	listener, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Fatal("network error:", err)
 	}
-	log.Println("start rpc server on", listener.Addr())
+	log.Println("start rpc on", listener.Addr())
 	addr <- listener.Addr().String()
 	g33rpc.Accept(listener)
 }
@@ -24,21 +38,21 @@ func main() {
 	addr := make(chan string)
 	go startServer(addr)
 	client, _ := g33rpc.Dial("tcp", <-addr)
-	defer func() { _ = client.Close() }()
+	defer func() {_ = client.Close()}()
 
-	time.Sleep(time.Second * 5)
-	// send requeset & receive response
+	time.Sleep(time.Second)
+	// send request & receive response
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i ++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("g33rpc req %d : ", i)
-			var reply string
+			args := &Args{Num1 : i, Num2 : i * i}
+			var reply int
 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
-				log.Fatal("call Foo.Sum error", err)
+				log.Fatal("call Foo.Sum error")
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
--- server.go
+++ server.go
@@ -2,12 +2,13 @@ package g33rpc
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"github.com/SeanChan0901/g33rpc/serializer"
 	"io"
 	"log"
 	"net"
 	"reflect"
+	"strings"
 	"sync"
 )
 
@@ -23,13 +24,50 @@ var DefaultOption = &Option{
 	CodeType:    serializer.GobType,
 }
 
-type Server struct{}
+// Server represents an RPC Server.
+type Server struct{
+	serviceMap sync.Map
+}
 
 func NewServer() *Server {
 	return &Server{}
 }
 
 var DefaultServer = NewServer()
+
+// Register publishes in the server the set of methods of the receiver
+func (server *Server) Register(rcvr interface{}) error {
+	s := newService(rcvr)
+	if _, dup := server.serviceMap.LoadOrStore(s.name, s); dup {
+		return errors.New("rpc : service already defined :" + s.name)
+	}
+	return nil
+}
+
+// Register publishes the receiver's methods in the DefaultServer.
+func Register(rcvr interface{}) error {
+	return DefaultServer.Register(rcvr)
+}
+
+func (server *Server) findService(serviceMethod string) (svc *service, mtype *methodType, err error) {
+	dot := strings.LastIndex(serviceMethod, ".")
+	if dot < 0 {
+		err = errors.New("rpc server : service/method request ill-format: " + serviceMethod)
+		return
+	}
+	serviceName, methodName := serviceMethod[:dot], serviceMethod[dot + 1:]
+	svci, ok := server.serviceMap.Load(serviceName)
+	if !ok {
+		err = errors.New("rpc server : can't find service" + serviceName)
+		return
+	}
+	svc = svci.(*service)
+	mtype = svc.method[methodName]
+	if mtype == nil {
+		err = errors.New("rpc server : can't find method" + methodName)
+	}
+	return
+}
 
 func (server *Server) Accept(listener net.Listener) {
 	for {
@@ -54,13 +92,13 @@ func (server *Server) ServeConn(conn io.ReadWriteCloser) {
 	}
 
 	if opt.MagicNumber != MagicNumber {
-		log.Println("rpc server: invalid magic number %x", opt.MagicNumber)
+		log.Printf("rpc server: invalid magic number %x", opt.MagicNumber)
 		return
 	}
 
 	f := serializer.NewSerializerMap[opt.CodeType]
 	if f == nil {
-		log.Println("rpc server: invalid serializer type %s", opt.CodeType)
+		log.Printf("rpc server: invalid serializer type %s", opt.CodeType)
 		return
 	}
 
@@ -89,8 +127,10 @@ func (server *Server) serveHandler(ss serializer.Serializer) {
 }
 
 type request struct {
-	h *serializer.Header
-	argv, replyv reflect.Value
+	h 				*serializer.Header
+	argv, replyv 	reflect.Value
+	mtype 			*methodType
+	svc 			*service
 }
 
 func (server *Server) readRequestHeader(ss serializer.Serializer) (*serializer.Header, error) {
@@ -110,12 +150,26 @@ func (server *Server) readRequest(ss serializer.Serializer) (*request, error) {
 		return nil, err
 	}
 
-	req := request{h : h}
-	req.argv = reflect.New(reflect.TypeOf(""))
-	if err := ss.ReadBody(req.argv.Interface()); err != nil {
-		log.Println("rpc server: read argv err:", err)
+	req := &request{h : h}
+	req.svc, req.mtype, err = server.findService(h.ServiceMethod)
+	if err != nil {
+		return req, err
 	}
-	return &req, nil
+	req.argv = req.mtype.newArgv()
+	req.replyv = req.mtype.newReplyv()
+
+	// make sure that argvi is a pointer, ReadBody need a pointer as parameter
+	argvi := req.argv.Interface()
+	if req.argv.Type().Kind() != reflect.Ptr {
+		argvi = req.argv.Addr().Interface()
+	}
+
+	if err = ss.ReadBody(argvi); err != nil {
+		log.Println("rpc server : read body err : ", err)
+		return req, err
+	}
+
+	return req, nil
 }
 
 func (server *Server) sendResponse(ss serializer.Serializer, h *serializer.Header, body interface{}, sending *sync.Mutex) {
@@ -128,7 +182,11 @@ func (server *Server) sendResponse(ss serializer.Serializer, h *serializer.Heade
 
 func (server *Server) handleRequest(ss serializer.Serializer, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
 	defer wg.Done()
-	log.Println(req.h, req.argv.Elem())
-	req.replyv = reflect.ValueOf(fmt.Sprintf("g33rpc response : %d", req.h.Seq))
+	err := req.svc.call(req.mtype, req.argv, req.replyv)
+	if err != nil {
+		req.h.Error = err.Error()
+		server.sendResponse(ss, req.h, invalidRequest, sending)
+		return
+	}
 	server.sendResponse(ss, req.h, req.replyv.Interface(), sending)
 }
